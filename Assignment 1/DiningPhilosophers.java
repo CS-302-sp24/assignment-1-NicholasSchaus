28627d97fa38374014fd971a66f646582a57255a
NicholasSchaus <nicholas.schaus@mnsu.edu>
2024-02-10 10:01:52 -0600

    "First commit, saving before testing thread idea"

--- Assignment 1/DiningPhilosophers.java
+++ Assignment 1/DiningPhilosophers.java
@@ -1,16 +1,95 @@
+import java.io.File;  // Import the File class
+import java.io.FileWriter;  // Import FileWriter Class
+import java.io.IOException;  // Import the IOException class to handle errors
+
+
+
 public class DiningPhilosophers {
 
-  public static void main(String[] args) throws InterruptedException {
-    Philosopher[] philosophers = new Philosopher[5];
-    Chopstick[] chopsticks = new Chopstick[5];
+  public static void main(String[] args) throws InterruptedException, IOException {
+    // For testing, if no inputs provided
+    if(args.length == 0) {
+      String[] temp = {"2", "3", "500", "500", "0"};
+      args = temp;
+    }
+
+    //Create output File
+    try {
+      File Trace = new File("Trace.txt");
+      if (Trace.createNewFile()) {
+        System.out.println("File created: " + Trace.getName());
+      } else { 
+        //If file already exists, recreate file to reset log
+        System.out.println("File already exists.");
+        Trace.delete();
+        Trace.createNewFile();
+        System.out.println("File reset: " + Trace.getName());
+      }
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+
+    //Create file Writer (how to get around without try statement?)
+    FileWriter traceWriter = new FileWriter("Trace.txt");
+
+    // Set up Philosophers
+    int GatheringSize = 1; // *np* is the number of philosophers (and forks).
+    int MaxCycles = 1; // *nc* is the number of cycles that each philosopher will go through. If this parameter is 0, they run forever.
+    int MaxThinkingTime = 1; // *tt* is the maximum thinking time, in units of milliseconds.
+    int MaxEatingTime = 1;// *et* is the maximum eating time, in units of milliseconds.
+    int DominantHand = 0; // If *rl* is 0, then all philosophers are right-handed and try to grab their right chopstick first. If *rl* is 1, then even-numbered 
+    assert (args.length == 5);
+    try {
+      GatheringSize = Integer.valueOf(args[0]);
+      MaxCycles = Integer.valueOf(args[1]);
+      MaxThinkingTime = Integer.valueOf(args[2]);
+      MaxEatingTime = Integer.valueOf(args[3]);
+      DominantHand = Integer.valueOf(args[4]);
+    } catch (NumberFormatException n) {
+      System.out.println("Inputs invalid, must input numbers as arguments");
+      System.out.println(n.getStackTrace());
+      System.exit(-1);
+    }
+    assert (DominantHand == 1 || DominantHand == 0);
     
-    for (int i = 0; i < 5; ++i)
+
+
+
+    // Simulate philosophers
+    Philosopher[] philosophers = new Philosopher[GatheringSize];
+    Chopstick[] chopsticks = new Chopstick[GatheringSize];
+    
+    for (int i = 0; i < GatheringSize; ++i)
       chopsticks[i] = new Chopstick(i);
-    for (int i = 0; i < 5; ++i) {
-      philosophers[i] = new Philosopher(chopsticks[i], chopsticks[(i + 1) % 5]);
+    for (int i = 0; i < GatheringSize; ++i) {
+      if(DominantHand == 1) {
+        philosophers[i] = new Philosopher(Integer.toString(i), chopsticks[i], chopsticks[(i + 1) % GatheringSize], MaxCycles, MaxEatingTime, MaxThinkingTime, i%2==0, traceWriter);
+      }
+      else {
+        philosophers[i] = new Philosopher(Integer.toString(i), chopsticks[i], chopsticks[(i + 1) % GatheringSize], MaxCycles, MaxEatingTime, MaxThinkingTime, true, traceWriter);
+      }
       philosophers[i].start();
     }
-    for (int i = 0; i < 5; ++i)
+    for (int i = 0; i < GatheringSize; ++i)
       philosophers[i].join();
-  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    traceWriter.close();
+  } 
+
 }
--- Assignment 1/Philosopher.java
+++ Assignment 1/Philosopher.java
@@ -1,28 +1,207 @@
 import java.util.Random;
+import java.io.FileWriter;
+import java.io.IOException;
+
 
 class Philosopher extends Thread {
+  private String name;
   private Chopstick left, right;
+  private boolean rightHanded;
   private Random random;
   private int thinkCount;
+  private int MaxCycles;
+  private int MaxThinkingTime;
+  private int MaxEatingTime;
+  private FileWriter writer;
 
-  public Philosopher(Chopstick left, Chopstick right) {
-    this.left = left; this.right = right;
+  public Philosopher(String name, Chopstick left, Chopstick right, int MaxCycles, int MaxEatingTime, int MaxThinkingTime, boolean righthanded, FileWriter writer) {
+    // Oh man that got long, hopefully you dont run too many philosophers at once, might hog a lot of memory.
+    this.name = name;
+    this.left = left; 
+    this.right = right;
+    this.rightHanded = righthanded;
+    this.MaxCycles = MaxCycles;
+    this.MaxEatingTime = MaxEatingTime;
+    this.MaxThinkingTime = MaxThinkingTime;
+    this.writer = writer;
     random = new Random();
   }
 
   public void run() {
     try {
-      while(true) {
-        ++thinkCount;
-        if (thinkCount % 10 == 0)
-          System.out.println("Philosopher " + this + " has thought " + thinkCount + " times");
-        Thread.sleep(random.nextInt(1000));     // Think for a while
-        synchronized(left) {                    // Grab left chopstick 
-          synchronized(right) {                 // Grab right chopstick 
-            Thread.sleep(random.nextInt(1000)); // Eat for a while
+      int CurrentCycle = 0;
+      while(CurrentCycle < MaxCycles) {
+        // FINISH
+        long curTime = System.currentTimeMillis();
+
+        // Eating
+
+        // Grabbing chopsticks, (right hand dominant)
+        if(rightHanded) {
+
+          // Prints want right chopstick
+          try {
+            writer.write("Philosopher " + name + " wants right chopstick" + "\n");
+          } catch (IOException io) {
+            System.out.println("Failed to print line in Philosopher " + name);
+            io.printStackTrace(); }
+
+          // Grab right chopstick
+          synchronized(right) {
+
+            // Prints has right chopstick
+            try {
+              writer.write("Philosopher " + name + " has right chopstick" + "\n");
+            } catch (IOException io) {
+              System.out.println("Failed to print line in Philosopher " + name);
+              io.printStackTrace(); }
+            
+
+            //FINISH
+            //Pause to force deadlock
+            try {
+              writer.write("Philosopher " + name + " WAITING TO FORCE DEADLOCK" + "\n");
+            } catch (IOException io) {
+              System.out.println("Failed to print line in Philosopher " + name);
+              io.printStackTrace(); }
+            Thread.sleep(2000);
+
+
+            // Prints wants left chopstick
+            try {
+              writer.write("Philosopher " + name + " wants left chopstick" + "\n");
+            } catch (IOException io) {
+              System.out.println("Failed to print line in Philosopher " + name);
+              io.printStackTrace(); }
+            
+            // Grab Left chopstick
+            synchronized(left) {
+
+              // Prints has left chopstick
+              try {
+                writer.write("Philosopher " + name + " has left chopstick" + "\n");
+              } catch (IOException io) {
+                System.out.println("Failed to print line in Philosopher " + name);
+                io.printStackTrace(); }
+              
+              // prints time waited since last meal
+              long t = System.currentTimeMillis() - curTime;
+              try {
+                writer.write("Philosopher " + name + " thought for " + t + " units of time" + "\n");
+              } catch (IOException io) {
+                System.out.println("Failed to print line in Philosopher " + name);
+                io.printStackTrace();
+              }
+
+              // prints eat for a bit, then eats
+              int r = random.nextInt(MaxEatingTime);
+              try {
+                writer.write("Philosopher " + name + " eats for " + Integer.toString(r) + " units" + "\n");
+              } catch (IOException io) {
+                System.out.println("Failed to print line in Philosopher " + name);
+                io.printStackTrace();
+              }
+              Thread.sleep(r);
+
+              //Release chopsticks
+              try {
+                writer.write("Philosopher " + name + " releases left chopstick" + "\n");
+              } catch (IOException io) {
+                System.out.println("Failed to print line in Philosopher " + name);
+                io.printStackTrace();
+              }
+              try {
+                writer.write("Philosopher " + name + " releases right chopstick" + "\n");
+              } catch (IOException io) {
+                System.out.println("Failed to print line in Philosopher " + name);
+                io.printStackTrace();
+              }
+            }
           }
+          // Increment Cycle Count
+          ++CurrentCycle;
+
+        //Grabbing chopsticks (Left hand dominant)
+        } else {
+
+          // Prints want left chopstick
+          try {
+            writer.write("Philosopher " + name + " wants left chopstick" + "\n");
+          } catch (IOException io) {
+            System.out.println("Failed to print line in Philosopher " + name);
+            io.printStackTrace(); }
+
+          // Grab right chopstick
+          synchronized(left) {
+
+            // Prints has left chopstick
+            try {
+              writer.write("Philosopher " + name + " has left chopstick" + "\n");
+            } catch (IOException io) {
+              System.out.println("Failed to print line in Philosopher " + name);
+              io.printStackTrace(); }
+
+            // Prints wants left chopstick
+            try {
+              writer.write("Philosopher " + name + " wants right chopstick" + "\n");
+            } catch (IOException io) {
+              System.out.println("Failed to print line in Philosopher " + name);
+              io.printStackTrace(); }
+            
+            // Grab Left chopstick
+            synchronized(right) {
+
+              // Prints has right chopstick
+              try {
+                writer.write("Philosopher " + name + " has right chopstick" + "\n");
+              } catch (IOException io) {
+                System.out.println("Failed to print line in Philosopher " + name);
+                io.printStackTrace(); }
+
+              // prints time waited since last meal
+              long t = System.currentTimeMillis() - curTime;
+              try {
+                writer.write("Philosopher " + name + " thought for " + t + " units of time" + "\n");
+              } catch (IOException io) {
+                System.out.println("Failed to print line in Philosopher " + name);
+                io.printStackTrace();
+              }
+
+              // prints eat for a bit, then eats
+              int r = random.nextInt(MaxEatingTime);
+              try {
+                writer.write("Philosopher " + name + " eats for " + Integer.toString(r) + " units" + "\n");
+              } catch (IOException io) {
+                System.out.println("Failed to print line in Philosopher " + name);
+                io.printStackTrace();
+              }
+              Thread.sleep(r);
+
+              //Release chopsticks
+              try {
+                writer.write("Philosopher " + name + " releases left chopstick" + "\n");
+              } catch (IOException io) {
+                System.out.println("Failed to print line in Philosopher " + name);
+                io.printStackTrace();
+              }
+              try {
+                writer.write("Philosopher " + name + " releases right chopstick" + "\n");
+              } catch (IOException io) {
+                System.out.println("Failed to print line in Philosopher " + name);
+                io.printStackTrace();
+              }
+            }
+          }
+          // Increment Cycle Count
+          ++CurrentCycle;
         }
       }
+      try {
+        writer.write("Philosopher " + name + " has finished their meal!!!!!!!!!!!!!!!!!!!!!!" + "\n");
+      } catch (IOException io) {
+        System.out.println("Failed to print line in Philosopher " + name);
+        io.printStackTrace();
+      } 
     } catch(InterruptedException e) {}
   }
 }
